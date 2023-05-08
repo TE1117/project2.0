2c8c086aba9d7adae13a1d2d939c8d3d60877925
DESKTOP-FSSDO9O\s9803 <e010659@gmail.com>
2023-05-09 01:06:42 +0800

    傳送帶移動、玩具鋼體

---
+++ Assets/C#/sped.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sped : MonoBehaviour
+{
+    Rigidbody m_rigidbody;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 pos = m_rigidbody.position;
+        m_rigidbody.position += Vector3.left * speed * Time.fixedDeltaTime;
+        m_rigidbody.MovePosition(pos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+

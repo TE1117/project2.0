50dcdd9a1222618f9b466ce24d36c68e78d94663
tnt17 <e010659@gmail.com>
2023-05-09 18:16:19 +0800

    bullet pick/icon/ui

--- Assets/C#/UIManager.cs
+++ Assets/C#/UIManager.cs
@@ -5,17 +5,28 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public Text bullet;
+    public GameObject bullet;
+    public Image  but1,but2,but3,but4;
     // Start is called before the first frame update
     void Start()
     {
+        bullet.active =(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        bullet.text = GameManager.bullet.ToString();
+        //bullet.text = GameManager.bullet.ToString();
+        if (GameManager.bullet == 1)
+        {
+            bullet.gameObject.active = (true);
+            but1.gameObject.active = (true);
+        }
+        if (GameManager.bullet == 2) but2.gameObject.active = (true);
+        if (GameManager.bullet == 3) but3.gameObject.active = (true);
+        if (GameManager.bullet == 4) but4.gameObject.active = (true);
+
 
     }
 }
